e2df32bc91ef2497625c2d66f7453050991ad649
vijay mittal <vijaymittalhybrid@gmail.com>
2014-10-16 08:59:43 +0000

    Changes made by vijay mittal (vijaymittalhybrid@gmail.com).

--- OM Hospital/project/script/dashbord.js
+++ OM Hospital/project/script/dashbord.js
@@ -1,9 +1,39 @@
 (function(global){
-    var dashbordModel,
+    var dashbordModel,drawerSource,logoSource,
         app = global.app = global.app || {};
     
     dashbordModel = kendo.data.ObservableObject.extend({
-        show:function(e){
+        show:function(){
+            
+            /*Drawer*/
+             drawerSource = new kendo.data.DataSource({
+                transport:{
+                    read:{
+                        url:'project/data/drawerListData.json',
+                        dataType:'json'
+                    }
+                }
+            });
+            
+            var viewModel = kendo.observable({ drawer_items:drawerSource});
+            kendo.bind($("ul"), viewModel);
+            
+            /*logo*/
+            /*logoSource = new kendo.data.DataSource({
+                transport:{
+                    read:{
+                        url:'project/data/logoData.json',
+                        dataType:'json'
+                    }
+                }
+            });
+            
+            var logoModel = kendo.observable({ logo_item:logoSource});
+            kendo.bind($("#logo"), logoModel);*/
+        },
+        
+        navigation:function(e){
+            console.log(e);
         }
     });
     
